b8aff1edffa9f5b60f6294c377c7a89a88b6adc3
Samuel Dandy <sam.dandy@tigers.com>
2025-08-27 15:12:25 -0500

    test

--- etl_logic/NWS/weather.py
+++ etl_logic/NWS/weather.py
@@ -5,7 +5,7 @@ from database import database
 from etl_logic.nws.vars import wind_direction_weights
 from etl_logic.nws.utils import get_wind_speed_weight
 NWS_API_URL = "https://api.weather.gov/gridpoints/HGX/53,51/forecast/hourly"
-
+format_string = "%Y-%m-%dT%H:%M:%S"
 def fetch_weather_data(api_url, params=None):
     try:
         response = requests.get(api_url, params=params)
@@ -20,9 +20,8 @@ def transform_weather_data(data):
     periods = data.get("properties", {}).get("periods", [])
     weather_data = []
     for period in periods:
-
-        start_time_central = datetime.fromisoformat(period["startTime"]).replace(tzinfo=None)
-        end_time_central = datetime.fromisoformat(period["endTime"]).replace(tzinfo=None)
+        start_time_central = datetime.strptime(period["startTime"].replace("-05:00",""), format_string)
+        end_time_central = datetime.strptime(period["endTime"].replace("-05:00",""), format_string)
         wind_speed_weight = get_wind_speed_weight(period["windSpeed"].split(" ")[0])
         wind_direction_weight = wind_direction_weights.get(period["windDirection"], 0)
         weather_data.append({
